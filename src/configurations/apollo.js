c4cd745ae89e7cc94af26c027633a165b8b33ef4
Anh Tuan <anhtuan128@gmail.com>
2023-01-16 21:30:07 +1100

    change gql url

--- src/configurations/apollo.js
+++ src/configurations/apollo.js
@@ -2,7 +2,7 @@ import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-    uri: "https://paxform.com/graphql",
+    uri: "https://paxform.com/wp/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
