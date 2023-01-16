a7383437624a9aeed66b79515791346ce5daac40
Anh Tuan <anhtuan128@gmail.com>
2023-01-16 21:33:18 +1100

    revert to v1

--- src/configurations/apollo.js
+++ src/configurations/apollo.js
@@ -2,7 +2,7 @@ import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-    uri: "https://paxform.com/wp/graphql",
+    uri: "https://v1.paxfolio.com/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
