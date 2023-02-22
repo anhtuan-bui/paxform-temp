5c41db0410d91401c952e8b1841af489c8f4ef4b
anhtuan-bui <anhtuan.bui@paxform.com>
2023-02-22 14:14:00 +1100

    remove log

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 
-const secret = process.env["CLIENT_USERNAME"];
-console.log(secret);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
